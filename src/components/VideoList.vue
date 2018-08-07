70284479e311a08d00f3b6fa12eb3403e138c5fe
Tiffany Tang <amyamyxtiffany@gmail.com>
2018-08-07 12:02:01 -0700

    Add key property to listItem

--- src/components/VideoList.vue
+++ src/components/VideoList.vue
@@ -1,8 +1,9 @@
 <template>
   <ul>
     <VideoListItem 
-      v-for="video in videos" 
-      v-bind:video="video"
+      v-for="video in videos"
+      :video="video"
+      :key="video.etag"
     >
     </VideoListItem>
   </ul>
