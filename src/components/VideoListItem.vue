6f069c84e92376c5d5f2675051013898e34d3bb5
Tiffany Tang <amyamyxtiffany@gmail.com>
2018-08-07 12:18:59 -0700

    Add class names and thumbnails

--- src/components/VideoListItem.vue
+++ src/components/VideoListItem.vue
@@ -1,5 +1,7 @@
 <template>
-  <li>
+  <li class="list-group-item">
+    <img :src="thumbnailsUrl" />
+    <!-- <img v-bind:src="..." /> -->
     {{ video.snippet.title }}
   </li>
 </template>
@@ -9,6 +11,12 @@ export default {
   name: 'VideoListItem',
   props: {
     video: Object
+  },
+  computed: {
+    // thumbnailsUrl: function() {
+    thumbnailsUrl() {
+      return this.video.snippet.thumbnails.default.url;
+    }
   }
 };
 </script>
