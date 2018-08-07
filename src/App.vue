27ec1fbec1f863a551d94dd266f9d4550b47d144
Tiffany Tang <amyamyxtiffany@gmail.com>
2018-08-07 11:01:37 -0700

    Add youtube api key and axios get request upon searchTerm

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,9 @@
 </template>
 
 <script>
+import axios from 'axios';
 import SearchBar from './components/SearchBar';
+const API_KEY = 'AIzaSyBiN636z6nDsM-WwL0G21V12bx8UseWJHY';
 
 export default {
   name: 'App',
@@ -15,7 +17,14 @@ export default {
   }, 
   methods: {
     onTermChange: function(searchTerm) {
-      console.log(searchTerm);
+      axios.get('https://www.googleapis.com/youtube/v3/search', {
+        params: {
+          key: API_KEY,
+          type: 'video',
+          part: 'snippet',
+          q: searchTerm
+        }
+      }).then(response => console.log(response))
     }
   }
 };
